b037192881eb6d3706be7ca165d5dff0ab166084
Sandro <sandrovalgoi@gmail.com>
2022-04-15 00:22:12 -0300

    react-refresh

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,28 @@
+import { useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: 'nome do repo',
-    description: 'descrição do repo',
-    link: 'http://github.com/unform/unform'
-}
-
 export default function RepositoryList() {
+    const [repository, setRepository] = useState({
+        name: 'nome do repo',
+        description: 'descrição do repo',
+        link: 'http://github.com/unform/unform'
+    });
+
+    const handleClick = function(){
+        setRepository({...repository, name: repository.name + ' -- '})
+    }
     return (
         <section className="repository-list">
-            <h1>Lista de repositórios</h1>
+            <h1>Lista de repositórios Github</h1>
 
             <ul>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
             </ul>
+            <button onClick={
+                handleClick
+            }>teste</button>
         </section >
     );
 }
